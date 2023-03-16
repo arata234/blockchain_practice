42bb55fa8865615d36d18050194099a445cac1f9
arata234 <90789032+arata234@users.noreply.github.com>
2023-03-16 16:11:29 +0900

    add mining

--- blockchain.py
+++ blockchain.py
@@ -6,13 +6,20 @@ import hashlib
 
 import utils
 
+MINING_DIFFICULTY = 3
+MINING_SENDER_ADDRESS = "MASTER"
+MINING_REWARD = 1.0
+
+
 logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+logger = logging.getLogger(__name__)
 
 class BlockChain(object):
     
-    def __init__(self):
+    def __init__(self, blockchain_address=None):
         self.transaction_pool = []
         self.chain = []
+        self.blockchain_address = blockchain_address
         self.create_block(0, self.generate_hash({}))
         
     def create_block(self, nonce, previous_hash):
@@ -40,6 +47,38 @@ class BlockChain(object):
         self.transaction_pool.append(transaction)
         return True
 
+    def find_nonce(self, transactions, previous_hash, 
+                   nonce, difficulty=3):
+        block = utils.sorted_dict_by_key({
+            "transactions": transactions,
+            "previous_hash": previous_hash,
+            "nonce": nonce,
+        })
+        d_hash = self.generate_hash(block)
+        return d_hash[:difficulty] == "0"*difficulty
+        
+    
+    def proof_of_work(self):
+        transactions = self.transaction_pool.copy()
+        previous_hash = self.generate_hash(self.chain[-1])
+        nonce = 0
+        while self.find_nonce(transactions, previous_hash, nonce) is False:
+            nonce += 1
+        return nonce
+    
+    def mining(self):
+        self.add_transaction(
+            sender_blockchain_address=MINING_SENDER_ADDRESS,
+            recipient_blockchain_address=self.blockchain_address,
+            value=MINING_REWARD,
+        )
+        nonce=self.proof_of_work()
+        previous_hash=self.generate_hash(self.chain[-1])
+        self.create_block(nonce, previous_hash)
+        logger.info({"action":"mining", "status":"success"})
+        return True
+
+    
 def pprint(chains):
     for i, chain in enumerate(chains):
         print("="*25, "Chain ", i, "="*25)
@@ -55,16 +94,15 @@ def pprint(chains):
     print("*"*40)
         
 if __name__ == "__main__":
-    block_chain = BlockChain()
+    my_blockchain_address = "9dfvqao303kldaasldfiv0"
+    block_chain = BlockChain(my_blockchain_address)
     pprint(block_chain.chain)
     
     block_chain.add_transaction("A", "B", 1.0)
-    hash = block_chain.generate_hash(block_chain.chain[-1])
-    block_chain.create_block(240, hash)
+    block_chain.mining()
     pprint(block_chain.chain)
     
     block_chain.add_transaction("C", "D", 2.0)
-    block_chain.add_transaction("X", "Y", 1.0)
-    hash = block_chain.generate_hash(block_chain.chain[-1])
-    block_chain.create_block(100, hash)
+    block_chain.add_transaction("X", "Y", 3.0)
+    block_chain.mining()
     pprint(block_chain.chain)
